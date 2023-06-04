915bf85e89b24a020601817eaa49b2e8a85511f0
Quang <quangnguyenminh3152@gmail.com>
2023-06-04 20:50:15 +0700

    remove unnecessary file

--- recycle_bin/recycle/test 2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Đọc file csv và chỉ định các cột cần đọc
-df = pd.read_csv('result_nk/90_nodes/d90_3.csv', usecols=['STT', 'Best Solution', 'Best Tree', 'Time'])
-
-# Lưu các cột đã chọn vào file csv mới
-df.to_csv('result_nk/90_nodes/d90_3_new.csv', index=False)
